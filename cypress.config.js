02a59e43928415c0d8f79d62a27664e32ad7853f
leon.ji <leon.ji@doverunner.com>
2025-03-20 10:31:00 +0900

    add task

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,12 @@ module.exports ={
   e2e: {
     experimentalStudio: true,
     setupNodeEvents(on, config) {
+      on('task', {
+        generateLogFileName() {
+          const logFileName = `log_${Date.now()}.txt`;
+          return logFileName; // 로그 파일 이름 반환
+        },
+      });
    
     },
   },
