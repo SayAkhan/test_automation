560c6430533304fb1733d088e8f04538fbbe7796
Reon <jcg@inka.co.kr>
2025-04-17 09:59:34 +0900

    describe 추출하는 코드 추가

--- cypress.config.js
+++ cypress.config.js
@@ -24,6 +24,25 @@ module.exports = {
           const logFileName = `cypress_log_${formattedDate}.txt`;
           return logFileName; // 로그 파일 이름 반환
         },
+        getTestFromFile({ testNumber }) {
+          const fs = require('fs');
+          const path = require('path');
+          const filePath = path.join(__dirname, 'cypress', 'e2e', 'drm_auto_generated_tests.cy.js');
+          const fileContent = fs.readFileSync(filePath, 'utf8');
+          
+          // 정규식으로 해당 테스트 번호의 describe 블록을 찾음
+          const testPattern = `describe\\('${testNumber}_drm_[^']+', \\(\\) => \\{[\\s\\S]*?afterEach\\(function\\(\\) \\{[\\s\\S]*?\\}\\);[\\s\\S]*?\\}\\);`;
+          const regex = new RegExp(testPattern, 'g');
+          const matches = fileContent.match(regex);
+          
+          if (matches && matches.length > 0) {
+            return matches[0];
+          } else {
+            // 테스트를 찾지 못한 경우, 파일의 내용을 로그로 출력하여 디버깅
+            console.log(`Test ${testNumber} not found. File content:`, fileContent);
+            throw new Error(`Test ${testNumber} not found in drm_auto_generated_tests.cy.js`);
+          }
+        }
       });
     },
   },
